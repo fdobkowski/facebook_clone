77d9cdbdf0ef913d1618940fd2bf0b43edb42928
eDoZZ <florian.dobkowski@gmail.com>
2022-07-21 14:34:09 +0200

    added notification list styling

--- frontend/src/components/navbar/Navbar.js
+++ frontend/src/components/navbar/Navbar.js
@@ -6,6 +6,7 @@ import {useKeycloak} from "@react-keycloak/web";
 import {useCallback, useEffect, useRef, useState} from "react";
 import Searchbar from "./Searchbar";
 import {v4 as uuid} from 'uuid'
+import {useSelector} from "react-redux";
 
 const Navbar = ( { socket, setSocket }) => {
 
@@ -15,6 +16,7 @@ const Navbar = ( { socket, setSocket }) => {
     const [notifications, setNotifications] = useState([])
     const [notification_focus, setNotification_focus] = useState(false)
     const notification_ref = useRef(null)
+    const profiles = useSelector((state) => state.profiles.profiles)
 
     const handleLogout = useCallback(() => {
         if (keycloak.authenticated) keycloak.logout()
@@ -58,13 +60,24 @@ const Navbar = ( { socket, setSocket }) => {
                         {notifications.map(x => {
                             return (
                                 <li key={uuid()} id={x.type}>
-                                    {x.type}
+                                    {(x.type === 'friend_request') ?
+                                        <div className={'friend_request'}>
+                                            <img alt={'profile_picture'} src={require('../../assets/fb_profile_picture.png')} />
+                                            <div>
+                                                <span>{`${profiles.find(y => y.id === x.from).first_name} has sent you a friend request`}</span>
+                                                <div>
+                                                    <button>Accept</button>
+                                                    <button>Decline</button>
+                                                </div>
+                                            </div>
+                                        </div> : null
+                                    }
                                 </li>
                             )
                         })}
                     </ul> : null}
-                    <button onClick={() => (cookies['profile_id']) ? navigate(`/profile/${cookies['profile_id']}`) : null}>Profile</button>
-                    <button onClick={() => {
+                    <button className={'util_button'} onClick={() => (cookies['profile_id']) ? navigate(`/profile/${cookies['profile_id']}`) : null}>Profile</button>
+                    <button className={'util_button'} onClick={() => {
                         removeCookies('profile_id')
                         removeCookies('profile_first_name')
                         removeCookies('profile_last_name')
