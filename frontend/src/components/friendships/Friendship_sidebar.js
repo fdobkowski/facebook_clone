79a6945de1bc11d0ecba285e15c6c426d0458116
eDoZZ <florian.dobkowski@gmail.com>
2022-07-22 20:44:04 +0200

    added img onclick redirect to profile page + friendships sidebar styling correction

--- frontend/src/components/friendships/Friendship_sidebar.js
+++ frontend/src/components/friendships/Friendship_sidebar.js
@@ -2,12 +2,14 @@ import '../../styles/Friendship_sidebar.scss'
 import {useDispatch, useSelector} from "react-redux";
 import {useEffect} from "react";
 import {getFriendships} from "../../redux/reducers/profileReducer";
+import {useNavigate} from "react-router-dom";
 
 const Friendship_sidebar = ( { createPost, setCreatePost, id }) => {
 
     const all_profiles = useSelector((state) => state.profiles.profiles)
     const profile = useSelector((state) => state.profiles.profiles.find(x => x.id === id))
     const dispatch = useDispatch()
+    const navigate = useNavigate()
 
     useEffect(() => {
         dispatch(getFriendships(id))
@@ -22,7 +24,7 @@ const Friendship_sidebar = ( { createPost, setCreatePost, id }) => {
                     {profile.friendships.map((x, i) => {
                         return (
                             <li key={i}>
-                                <img alt={'profile_picture'} src={require('../../assets/fb_profile_picture.png')} />
+                                <img onClick={() => navigate(`/profile/${x.friend}`)} alt={'profile_picture'} src={require('../../assets/fb_profile_picture.png')} />
                                 <span>{all_profiles.find(y => y.id === x.friend).first_name} {all_profiles.find(y => y.id === x.friend).last_name}</span>
                             </li>
                         )
