d91dfca4fb8ea9e77a44d0069e34ae00d2881ccb
eDoZZ <florian.dobkowski@gmail.com>
2022-07-22 19:04:39 +0200

    fixed error when user types non existing email on login

--- backend/resource_api/login/login.js
+++ backend/resource_api/login/login.js
@@ -11,22 +11,23 @@ router.get("/", async (req, res) => {
 
     pool.query(queries.auth_user({login: login, password: password}), async (error, response) => {
         if (error) throw error
-        const decrypt = CryptoJS.AES.decrypt(response.rows[0].password, '274989hash')
-        if (password === decrypt.toString(CryptoJS.enc.Utf8)) {
-            const data = {}
-            await pool.query(queries.get_profile_id({login: login}), async (error, response) => {
-                if (error) throw error
-                data.id = response.rows[0].id
-                await pool.query(queries.get_profile_name(data.id), (error, response) => {
+        if (response.rows.length !== 0) {
+            const decrypt = CryptoJS.AES.decrypt(response.rows[0].password, '274989hash')
+            if (password === decrypt.toString(CryptoJS.enc.Utf8)) {
+                const data = {}
+                await pool.query(queries.get_profile_id({login: login}), async (error, response) => {
                     if (error) throw error
-                    data.first_name = response.rows[0].first_name
-                    data.last_name = response.rows[0].last_name
+                    data.id = response.rows[0].id
+                    await pool.query(queries.get_profile_name(data.id), (error, response) => {
+                        if (error) throw error
+                        data.first_name = response.rows[0].first_name
+                        data.last_name = response.rows[0].last_name
 
-                    res.status(200).send(data)
+                        res.status(200).send(data)
+                    })
                 })
-            })
-        }
-        else res.status(401).send("Wrong credentials")
+            } else res.status(401).send("Wrong credentials")
+        } else res.status(401).send("Wrong credentials")
     })
 })
 
