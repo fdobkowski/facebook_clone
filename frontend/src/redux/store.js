8da3d17a9823e855e4a68197abab8bd5025b4e9f
eDoZZ <florian.dobkowski@gmail.com>
2022-08-11 21:06:59 +0200

    added authenticatio reducer to redux

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,10 +1,12 @@
 import profileReducer from "./reducers/profileReducer";
 import postReducer from "./reducers/postReducer";
 import {configureStore} from "@reduxjs/toolkit";
+import authReducer from "./reducers/authReducer";
 
 export const store = configureStore({
     reducer: {
         profiles: profileReducer,
-        posts: postReducer
+        posts: postReducer,
+        auth: authReducer
     }
 })
